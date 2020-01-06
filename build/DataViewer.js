75de6b6827bbedacbd31c3fca4afae91b021006a
yangyxu <945758912@qq.com>
2020-01-06 16:12:43 +0800

    update DataViewer.js

--- build/DataViewer.js
+++ build/DataViewer.js
@@ -7,25 +7,29 @@ module.exports = React.createClass({
   getInitialState: function getInitialState() {
     return {
       data: null,
-      loading: false,
-      value: this.props.value,
-      values: []
+      loading: false
     };
   },
   componentDidMount: function componentDidMount() {
-    this.state.data = zn.data.create(this.props.data, {
-      before: function () {
+    var _events = this.props.events || {};
+
+    this.state.data = zn.data.create(this.props.data, zn.extend(_events, {
+      before: function (sender, data) {
         this.setState({
           loading: true
         });
+        this.props.onLoading && this.props.onLoading(data, this);
+        _events.before && _events.before(sender, data);
       }.bind(this),
       after: function (sender, data) {
         this.setState({
           loading: false,
           data: data
         });
+        this.props.onFinished && this.props.onFinished(data, this);
+        _events.after && _events.after(sender, data);
       }.bind(this)
-    });
+    }), this.props.context);
   },
   componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
     if (nextProps.data !== this.props.data) {
@@ -38,9 +42,29 @@ module.exports = React.createClass({
   render: function render() {
     var _data = this.state.data;
 
+    if (this.state.loading) {
+      var _return = this.props.loadingRender && this.props.loadingRender(this);
+
+      if (_return) {
+        return _return;
+      }
+    }
+
     if (_data && _data.length) {
+      var _return = this.props.dataRender && this.props.dataRender(_data, this);
+
+      if (_return) {
+        return _return;
+      }
+
       return React.createElement(React.Fragment, null, _data.map(this.__itemRender));
-    } else {
+    } else if (!this.state.loading) {
+      var _return = this.props.emptyRender && this.props.emptyRender();
+
+      if (_return) {
+        return _return;
+      }
+
       return React.createElement(React.Fragment, null);
     }
   }
--- src/DataViewer.js
+++ src/DataViewer.js
@@ -5,25 +5,28 @@ module.exports = React.createClass({
 	getInitialState:function(){
 		return {
 			data: null,
-			loading: false,
-			value: this.props.value,
-			values: []
+			loading: false
 		}
 	},
 	componentDidMount:function(){
-		this.state.data = zn.data.create(this.props.data, {
-			before: function (){
+		var _events = this.props.events || {};
+		this.state.data = zn.data.create(this.props.data, zn.extend(_events, {
+			before: function (sender, data){
 				this.setState({
 					loading: true
 				});
+				this.props.onLoading && this.props.onLoading(data, this);
+				_events.before && _events.before(sender, data);
 			}.bind(this),
 			after: function (sender, data){
 				this.setState({
 					loading: false,
 					data: data
-				})
+				});
+				this.props.onFinished && this.props.onFinished(data, this);
+				_events.after && _events.after(sender, data);
 			}.bind(this)
-		});
+		}), this.props.context);
 	},
 	componentWillReceiveProps: function(nextProps){
 		if(nextProps.data!==this.props.data){
@@ -35,7 +38,19 @@ module.exports = React.createClass({
 	},
 	render: function(){
 		var _data = this.state.data;
+		if(this.state.loading){
+			var _return = this.props.loadingRender && this.props.loadingRender(this);
+			if(_return) {
+				return _return;
+			}
+		}
 		if(_data && _data.length){
+			var _return = this.props.dataRender && this.props.dataRender(_data, this);
+			
+			if(_return) {
+				return _return;
+			}
+			
 			return (
 				<>
 					{
@@ -43,7 +58,12 @@ module.exports = React.createClass({
 					}
 				</>
 			);
-		}else{
+		} else if(!this.state.loading){
+			var _return = this.props.emptyRender && this.props.emptyRender();
+			if(_return) {
+				return _return;
+			}
+
 			return <></>;
 		}
 	}
