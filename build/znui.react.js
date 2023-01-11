b27ff51997f8a10be4499b7aa766b6c59a7bb15f
yangyxu <yangyxu@cisco.com>
2023-01-11 19:10:42 +0800

    rebuild

--- build/znui.react.js
+++ build/znui.react.js
@@ -23,6 +23,46 @@ module.exports = znui.react = {
   setting: {
     _data_: {}
   },
+  versions: function versions() {
+    var u = navigator.userAgent;
+    return {
+      trident: u.indexOf('Trident') > -1,
+      //IE内核
+      presto: u.indexOf('Presto') > -1,
+      //opera内核
+      gecko: u.indexOf('Firefox') > -1,
+      //火狐内核Gecko
+      safari: u.indexOf('Safari') > -1,
+      //Safari
+      chrome: u.indexOf('Chrome') > -1,
+      //Safari
+      mobile: !!u.match(/AppleWebKit.*Mobile.*/),
+      //是否为移动终端
+      ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+      //ios
+      android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1,
+      //android
+      iPhone: u.indexOf('iPhone') > -1,
+      //iPhone
+      iPad: u.indexOf('iPad') > -1,
+      //iPad
+      webKit: u.indexOf('AppleWebKit') > -1,
+      //苹果、谷歌内核
+      webApp: u.indexOf('Safari') > -1 //Safari
+
+    };
+  },
+  nowdateString: function nowdateString(timestamp) {
+    var _timestamp = timestamp || zn.date.nowTimeString();
+
+    var _versions = znui.react.versions();
+
+    if (!_versions.chrome && _versions.safari) {
+      return zn.date.nowDateString('-') + 'T' + _timestamp;
+    }
+
+    return zn.date.nowDateString('-') + ' ' + _timestamp;
+  },
   arrayToValueMap: function arrayToValueMap(ary, valueKey, labelKey) {
     if (!zn.is(ary, 'array')) {
       throw new Error('data is not array.');
